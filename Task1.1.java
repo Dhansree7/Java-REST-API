5514b9590f88fac00642a51fa5d92cd66523439b
Dhansree7 <91130726+Dhansree7@users.noreply.github.com>
2022-04-16 12:04:04 +0530

    Add files via upload

---
+++ Task1.1.java
@@ -0,0 +1,11 @@
+java1({
+type:"GET",
+url:'test1.php',
+Num: {name:"my centos",id:"12",language:"Java",framework:"django"}
+Num1: {name:"my centos1",id:"13",language:"C",framework:"django1"}
+Num2: {name:"my centos2",id:"14",language:"Python",framework:"django2"}
+alert(Num);
+alert(Num1);
+alert(Num2);
+}
+})
---
+++ Task1.2.java
@@ -0,0 +1,7 @@
+java2({
+type:"POST",
+url:'test1.php',
+Num2: {name:"my centos2",id:"14",language:"Python",framework:"django2"}
+alert(Num2);
+}
+})
